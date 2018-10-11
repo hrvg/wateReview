f21d5b4bde69a10dd98f626bc0635ea087123446
hrvg <herve@guillon.xyz>
2018-10-10 19:34:50 -0700

    emails extraction

---
+++ R/utils/get_emails.R
@@ -0,0 +1,49 @@
+#####################
+##### LIBRARIES #####
+#####################
+
+library(pdftools)
+
+#################
+##### UTILS #####
+#################
+
+import::here(.from = "./R/utils/envpath.R", 
+	get_rootdir)
+
+########################
+##### INITIALIZING #####
+########################
+
+root.dir <- get_rootdir()
+
+################
+##### MAIN #####
+################
+
+get_mail <- function(pdf){
+	text <- pdf_text(pdf)
+	text2 <- strsplit(text, "\n")
+	i1 <- grep("@", text2)
+	if (length(i1) >= 1) {
+		mails <- lapply(i1, function(ii){
+			i2 <- grep("@", text2[[ii]])
+			text3 <- lapply(i2, function(i) unlist(strsplit(text2[[ii]][[i]], " ")))
+			mail <- lapply(text3, function(txt) txt[grep("@", txt)])
+			mail <- gsub("\r", "", mail)
+			mail <- gsub("\n", "", mail)
+			return(mail)
+		})
+		return(unlist(mails))
+	} else {
+		warning("More than one email address! (or less than one email address)")
+		return(length(i1))
+	}
+}
+
+lang <- "portuguese"
+pdf.dir <- paste0("data/latin_america/corpus_pdf/", lang, "/", lang, ".Data")
+
+lf <- list.files(file.path(root.dir, pdf.dir), recursive = TRUE, pattern = ".pdf", full.names = TRUE)
+mails <- unlist(lapply(lf, get_mail))
+write.table(mails, file.path(root.dir, pdf.dir, paste0("mails_", lang, ".csv")), row.names = FALSE, col.names = FALSE)
---
+++ src/get_emails.py
@@ -0,0 +1,28 @@
+from validate_email import validate_email
+
+def flatten(var,vartype='list'):
+	if vartype == 'list':
+		var=[item for sublist in var for item in sublist]
+	elif vartype == 'array':
+		var=np.asarray([item for sublist in var for item in sublist])
+	return var
+
+lang = "spanish"
+file = "./data/mails_" + lang +".csv"
+f = open(file,'r') # open file
+data_raw = f.readlines() # read lines, return a list, each element is a str
+f.close()
+data = [row.split('\r\n') for row in data_raw]
+data = [row.split('"') for row in flatten(data)]
+data = [row.split('(') for row in flatten(data)]
+data = [row.split(')') for row in flatten(data)]
+data = [row.split('\\') for row in flatten(data)]
+data = [row.split(',') for row in flatten(data)]
+data = [row.split(';') for row in flatten(data)]
+data = [row.split('*') for row in flatten(data)]
+data = flatten(data)
+
+mails = [elmt+'\n' for elmt in data if validate_email(elmt)]
+filename = open("./data/mails_" + lang +"_fmt.d",'w')
+filename.writelines(mails)
+filename.close()
