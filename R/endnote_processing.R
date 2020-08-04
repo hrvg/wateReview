3b7379087784375f1bc886505ee27469398f66d0
hrvg <herve@guillon.xyz>
2020-08-04 14:07:11 -0700

    Added vignette for EndNote formatting, deleted legacy code, added library of functions for EndNote formatting

--- R/endnote_processing.R
+++ R/endnote_processing.R
@@ -1,3 +1,8 @@
+#' Parse the .xml database from EndNote
+#' @param language character, one of "english", "portuguese", "spanish"
+#' @return xmldf, a data.frame with the data extracted by EndNote
+#' @import XML
+#' @export
 get_endnote_xml <- function(language){
 	pdf.dir <- paste0("data/latin_america/corpus_pdf/", language)	
 	doc <- xmlParse(file.path(root.dir, pdf.dir, paste0(language, "_database.xml")))
@@ -16,10 +21,12 @@ get_endnote_xml <- function(language){
 	return(xmldf)
 }
 
+#' Retrieve the titles of the documents in the English corpus
+#' @param language character, one of "english", "portuguese", "spanish"
+#' @return 
 get_endnote_titles <- function(language){
 	csv.dir <- paste0("data/latin_america/corpus_csv/", language, "/")
 	csv.file <- paste0(language, '_title_all.txt')
 	endnote_titles <- readLines(file.path(root.dir, csv.dir, csv.file))
 	return(endnote_titles)
 }
-
