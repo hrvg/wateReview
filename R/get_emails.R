b828efe1a5ed4767430e1ac2ce2b2e674f277a43
hrvg <herve@guillon.xyz>
2020-08-04 14:51:48 -0700

    Added email addresses extraction

--- R/get_emails.R
+++ R/get_emails.R
@@ -1,87 +1,31 @@
-#####################
-##### LIBRARIES #####
-#####################
-
-library(pdftools)
-
-#################
-##### UTILS #####
-#################
-
-import::here(.from = "./R/utils/envpath.R", 
-	get_rootdir)
-
-########################
-##### INITIALIZING #####
-########################
-
-# root.dir <- get_rootdir()
-root.dir <- "/media/hguillon/Seagate Expansion Drive" # dirty fix, see disclaimer
-
-
-################
-##### MAIN #####
-################
-
-
-#####################################
-########### DISCLAIMER ##############
-#####################################
-
-# 
-# You want to execute this code on a linux server to avoid the shitshow that is special character handling on Windows
-#
-
+#' Extract email addresses from `pdf` documents
+#' You probably want to execute this code on a linux server to avoid the issues with special character handling on Windows
+#' @param pdf path to a `.pdf` files
+#' @return emails addresses found in the `.pdf` document
+#' @export
 get_mail <- function(pdf){
 	# print(pdf)
 	if (file.size(pdf) > 0){
-
-	# if (lang == "spanish"){
-	# 	pdf <- gsub("i´", "í", pdf)
-	# 	pdf <- gsub("e´", "é", pdf)
-	# }
-	text <- pdf_text(pdf)
-	text2 <- strsplit(text, "\n")
-	i1 <- grep("@", text2)
-	if (length(i1) >= 1) {
-		mails <- lapply(i1, function(ii){
-			i2 <- grep("@", text2[[ii]])
-			text3 <- lapply(i2, function(i) unlist(strsplit(text2[[ii]][[i]], " ")))
-			mail <- lapply(text3, function(txt) txt[grep("@", txt)])
-			mail <- gsub("\r", "", mail)
-			mail <- gsub("\n", "", mail)
-			return(mail)
-		})
-		return(unlist(mails))
-	} else {
-		warning("More than one email address! (or less than one email address)")
-		return(length(i1))
+		# if (lang == "spanish"){
+		# 	pdf <- gsub("i´", "í", pdf)
+		# 	pdf <- gsub("e´", "é", pdf)
+		# }
+		text <- pdf_text(pdf)
+		text2 <- strsplit(text, "\n")
+		i1 <- grep("@", text2)
+		if (length(i1) >= 1) {
+			mails <- lapply(i1, function(ii){
+				i2 <- grep("@", text2[[ii]])
+				text3 <- lapply(i2, function(i) unlist(strsplit(text2[[ii]][[i]], " ")))
+				mail <- lapply(text3, function(txt) txt[grep("@", txt)])
+				mail <- gsub("\r", "", mail)
+				mail <- gsub("\n", "", mail)
+				return(mail)
+			})
+			return(unlist(mails))
+		} else {
+			warning("More than one email address! (or less than one email address)")
+			return(length(i1))
+		}
 	}
 }
-}
-
-lang <- "portuguese"
-
-# pdf.dirs <- c(
-# 	paste0("data/latin_america/corpus_pdf/", lang, "/", lang, ".Data"),
-# 	paste0("data/latin_america/corpus_pdf/", lang, "/manual_download_", lang)
-# 	)
-
-# more dirty fixes, see disclaimer
-pdf.dirs <- c(
-	paste0("latin_america/corpus_pdf/", lang, "/", lang, ".Data"),
-	paste0("latin_america/corpus_pdf/", lang, "/manual_download_", lang)
-	)
-
-lf <- lapply(pdf.dirs, function(pdf.dir){
-	list.files(file.path(root.dir, pdf.dir), recursive = TRUE, pattern = ".pdf", full.names = TRUE)
-})
-lf <- c(lf[[1]], lf[[2]])
-
-
-
-
-mails <- unlist(lapply(lf, get_mail))
-
-
-write.table(mails, file.path(root.dir, pdf.dirs[1], paste0("mails_", lang, ".csv")), row.names = FALSE, col.names = FALSE)
