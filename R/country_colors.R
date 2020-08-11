b237295db1f5e62b6024dac99b0584f71cfee181
hrvg <herve@guillon.xyz>
2020-08-11 15:40:48 -0700

    Added docs for country colors

--- R/country_colors.R
+++ R/country_colors.R
@@ -1,8 +1,19 @@
+#' Convert rgb to hex colors
+#' @param r red value
+#' @param g green value
+#' @param b blue value
+#' @return hex code
+#' @export
 rgb2hex <- function(r,g,b) sprintf('#%s',paste(as.hexmode(c(r,g,b)),collapse = ''))
 
+#' Get the dominant colors from country flags
+#' @param iso ISO Alpha-2 code
+#' @param n number of colors
+#' @return top `n` colors
+#' @export
 get_topColors <- function(iso, n = 3){
 	source <- paste0("../country-flags/png100px/", iso, ".png")
-	img <- readPNG(source, native = FALSE, info = FALSE)
+	img <- png::readPNG(source, native = FALSE, info = FALSE)
 	red <- c(img[,, 1] * 255)
 	green <- c(img[,, 2] * 255)
 	blue <- c(img[,, 3] * 255)
